d93461e40eec472e38c68ca30e36c475ea6b4080
1846102760@qq.com <1846102760@qq.com>
2020-06-29 15:29:01 +0800

    将spring-boot添加到git

---
+++ springBoot_demo/springboot_quick/src/main/java/cn/itheima/MySpringBootApplication.java
@@ -0,0 +1,13 @@
+package cn.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//声明该类是一个springboot的引导类
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        //run方法 表示运行springboot的引导类，run参数就是springBoot引导类的字节码对象
+       SpringApplication.run(MySpringBootApplication.class);
+    }
+}
